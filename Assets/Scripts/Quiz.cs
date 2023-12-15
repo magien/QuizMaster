14848492bf0c6a9ecdafd97304b63ac8b0f531f3
Cengizhan Dursun <cengizhandursun97@gmail.com>
2023-12-15 18:00:12 +0300

    somemorechanges

---
+++ Assets/Scripts/Quiz.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Quiz : MonoBehaviour
+{
+    [SerializeField]TextMeshProUGUI questionText;
+    [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerButtons;
+    int correctAnswerIndex;
+    [SerializeField]Sprite defaultAnswerSprite;
+    [SerializeField]Sprite correctAnwswerSprite;
+    void Start()
+    {
+        questionText.text = question.GetQuestion();
+
+        for(int i=0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
+
+    }
+    public void onAnswerSelected(int index)
+    {
+        Image buttonImage;
+        if (index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct!";
+            buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnwswerSprite;
+        }
+        else
+        {
+            correctAnswerIndex = question.GetCorrectAnswerIndex();
+            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            questionText.text = "Sorry correct answer was; \n" + correctAnswer;
+            buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnwswerSprite;
+            
+        }
+    }
+
+
+
+}
